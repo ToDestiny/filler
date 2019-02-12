08afbc20dd77e44c4a95c34e6a0354976170df1c
Anthony COLAS <acolas@e2r5p13.42.fr>
2019-02-12 14:49:13 +0100

    main leaks && gitignore

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 12:11:22 by acolas            #+#    #+#             */
-/*   Updated: 2019/02/04 23:55:05 by acolas           ###   ########.fr       */
+/*   Updated: 2019/02/05 16:50:32 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,14 +46,24 @@ void	ft_read_map(t_map *map)
 	{
 		get_next_line(0, &s);
 		if (!(map->m[i] = (char *)ft_memalloc(sizeof(char) * (map->w + 1))))
-			exit (0);
+			break;
 		if (ft_strchr(s, ' ') == 0)
-			exit (0);
+			break;
 		else
 			ft_strcpy(map->m[i], ft_strchr(s, ' ') + 1);
 		free(s);
 		i++;
 	}
+	if (i != map->h)
+	{
+		while (i--)
+			free(map->m[i]);
+		free(map);
+		free(s);
+		exit(EXIT_FAILURE);
+
+	}
+	map->m[i] = NULL;
 }
 
 void	ft_read_piece(t_map *map)
@@ -68,24 +78,36 @@ void	ft_read_piece(t_map *map)
 	{
 		get_next_line(0, &s);
 		if (!(map->p[i] = ft_strdup(s)))
-				exit (0);
+			break;
 		free(s);
 		i++;
 	}
+	if (i != map->ph)
+	{
+		while(i--)
+			free(map->ph[i]);
+		free(map);
+		free(s);
+		exit(EXIT_FAILURE);
+	}
+	map->ph[i] = NULL;
 }
 
-void	ft_read(t_map *map)
+static void ft_exit(char *s, char **a)
 {
-	char	*s;
-	char	**a;
+	free(s);
+	ft_free_arr(a);
+	exit(EXIT_FAILURE);
+}
 
+void	ft_read(t_map *map, char *s, char **a)
+{
 	while (get_next_line(0, &s) > -1)
 	{
 		if (ft_strncmp(s, "Plateau", 7) == 0)
 		{
-			a = ft_strsplit(s, ' ');
-			if (!(a[1]) || !(a[2]))
-				exit(0);
+			if ((a = ft_strsplit(s, ' ')) && (!(a[1]) || !(a[2])))
+				ft_exit(s, a);
 			map->h = ft_atoi(a[1]);
 			map->w = ft_atoi(a[2]);
 			ft_read_map(map);
@@ -93,9 +115,8 @@ void	ft_read(t_map *map)
 		}
 		if (ft_strncmp(s, "Piece", 5) == 0)
 		{
-			a = ft_strsplit(s, ' ');
-			if (!(a[1]) || !(a[2]))
-				exit (0);
+			if ((a = ft_strsplit(s, ' ')) && (!(a[1]) || !(a[2])))
+				ft_exit(s, a);
 			map->ph = ft_atoi(a[1]);
 			map->pw = ft_atoi(a[2]);
 			ft_read_piece(map);
@@ -129,6 +150,6 @@ int		main(void)
 		map.oc = 'O';
 	}
 	free(s);
-	ft_read(&map);
+	ft_read(&map, NULL, NULL);
 	return (1);
 }
